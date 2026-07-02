0cf3010f16a48cdf262f84014ead8504639ebce7
Mayur Vijay Sawarbandhe <mayursawarbandhe4@gmail.com>
2026-07-02 22:05:19 +0530

    Add files via upload

---
+++ Login.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import API from "../services/api";
+import { useNavigate, Link } from "react-router-dom";
+import "./Login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await API.post("/auth/login", {
+        email,
+        password,
+      });
+
+      alert(res.data.message);
+
+      localStorage.setItem("token", res.data.token);
+
+      navigate("/dashboard");
+    } catch (error) {
+      alert(error.response?.data?.message || "Login Failed");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <h1>Dairy Management System</h1>
+        <p className="subtitle">Sign in to continue</p>
+
+        <form onSubmit={handleLogin}>
+          <input
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type="submit">Login</button>
+        </form>
+
+        <p className="register-text">
+          Don't have an account?{" "}
+          <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
